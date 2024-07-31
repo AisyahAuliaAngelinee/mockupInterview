65fe4a1aaad466f9e9f877edaa2fe793f47a7b2e
アンジェリン <vincentiusclarishna@Vincentiuss-MacBook-Pro.local>
2024-07-31 22:02:55 +0700

    feat: clerk setup

---
+++ ai-interview/app/dashboard/layout.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DashboardLayout = ({ children }: any) => {
+	return (
+		<React.Fragment>
+			<div>{children}</div>
+		</React.Fragment>
+	);
+};
+
+export default DashboardLayout;
